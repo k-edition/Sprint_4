b82fd76630d9ca8cc057789aabfed6f8d7055aee
Vera Kuzmina <chat.vera.gpt@mail.ru>
2024-03-27 00:23:42 +0300

    create 10 tests in tests.py and create README

--- tests.py
+++ tests.py
@@ -1,24 +1,75 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
+    @pytest.mark.parametrize("name", ['t', 'Элегантность ежика', 'Сказка о спящей царевне и семи богатырях'])
+    def test_add_new_book_correct_symbol_quantity(self, name):
         collector = BooksCollector()
+        collector.add_new_book(name)
+        assert len(collector.get_books_genre()) == 1
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_set_book_genre_real_genre(self):
+        collector = BooksCollector()
+        name = 'Смилла и её чувство снега'
+        genre = 'Детективы'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_book_genre(name) == genre
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    def test_get_book_genre_new_book(self):
+        collector = BooksCollector()
+        name = 'Чапаев и пустота'
+        collector.add_new_book(name)
+        assert collector.get_book_genre(name) == ''
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_get_books_with_specific_genre_real_genre(self):
+        collector = BooksCollector()
+        name = 'История одного города'
+        genre = 'Комедии'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_with_specific_genre(genre) == [name]
+
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+        name = 'История одного города'
+        genre = 'Комедии'
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_genre() == collector.books_genre
+
+    @pytest.mark.parametrize('name,genre', [['1984','Фантастика'], ['Маугли','Мультфильмы'], ['12 стульев', 'Комедии']])
+    def test_get_books_for_children_allowed_genre(self, name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(name)
+        collector.set_book_genre(name, genre)
+        assert collector.get_books_for_children() == [name]
+
+    def test_add_book_in_favorites_repeat_book(self):
+        collector = BooksCollector()
+        name = 'Чапаев и пустота'
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        collector.add_book_in_favorites(name)
+        assert len(collector.get_list_of_favorites_books()) == 1
+
+    def test_delete_book_from_favorites_real_book(self):
+        collector = BooksCollector()
+        name = 'Чапаев и пустота'
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        collector.delete_book_from_favorites(name)
+        assert name not in collector.get_list_of_favorites_books()
+
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+        name = 'Элегантность ежика'
+        collector.add_new_book(name)
+        collector.add_book_in_favorites(name)
+        assert collector.get_list_of_favorites_books() == [name]
+
+    def test_default_value_books_genre(self):
+        collector = BooksCollector()
+        assert collector.get_books_genre() == {}
